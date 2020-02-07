b8b1c692f5891d2467147ca3c64fe0d6aa5e805e
mahetha25 <57928848+mahetha25@users.noreply.github.com>
2020-02-07 11:52:13 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+
+app.get('/', function (req, res) {
+   res.sendFile( __dirname + "/" + "main.html" );
+})
+
+serv.listen(process.env.PORT);
+console.log("Server is listening on port " + process.env.PORT);
